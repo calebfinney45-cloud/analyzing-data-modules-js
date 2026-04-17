db11fe85e6ef05641eb7dad3de3ae84ba0e49c28
Caleb Finney <calebfinney45@gmail.com>
2026-04-17 04:20:08 +0300

    Complete

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: Date.today().toString("M/d/yyyy")
+  }
 
+  // Loop through args and merge all arrays
+  for (let arrays of args) {
+    combinedObject.users.push(...arrays);
+  }
 
+  return combinedObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
